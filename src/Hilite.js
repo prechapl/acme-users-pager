950d37d026083becbccf7da4ce98c3d3b4d8f69d
Preston Chaplin <preston.chaplin@gmail.com>
2019-04-29 10:56:49 -0400

    api routes taken down before hilite feature completed

--- src/Hilite.js
+++ src/Hilite.js
@@ -1,26 +1,5 @@
 import React, { Component } from 'react';
 
-// const Hilite = ({ input, match }) => {
-//   const query = match.params.searchTerm;
-//   const split = input.split(query);
-//   return (
-//     <td>
-//       {split.length > 1
-//         ? split.map((word, idx) => (
-//             <span key={word}>
-//               {word}
-//               {idx !== split.length - 1 ? (
-//                 <span className="bg-warning">{query}</span>
-//               ) : (
-//                 ''
-//               )}
-//             </span>
-//           ))
-//         : split[0]}
-//     </td>
-//   );
-// };
-
 class Hilite extends Component {
   constructor(props) {
     super(props);
@@ -36,19 +15,19 @@ class Hilite extends Component {
       //   .split('.')
       //   .join(',')
       //   .split(',');
+
       let inputSplit = input.split(searchTerm);
 
-      console.log('props in Hilite render', this.props);
-      console.log('inputSplit', inputSplit);
+      // console.log('props in Hilite render', this.props);
+      // console.log('inputSplit', inputSplit);
 
       const match = inputSplit.filter(word => word === searchTerm);
 
       return inputSplit.forEach(text => {
-        match.includes(text) ? (
-          <span className="bg-warning">{text}</span>
-        ) : null;
+        return <span className="bg-warning">{text}</span>;
       });
     }
+
     return input;
   }
 }
